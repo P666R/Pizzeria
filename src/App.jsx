bfc7e7d5b505eb9ff64f899c31b32753b9dea863
PRoy <sam469roger@gmail.com>
2024-06-11 23:20:48 +0530

    implemented all the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import Home from './ui/Home.jsx';
 import Menu from './features/menu/Menu.jsx';
+import Cart from './features/cart/Cart.jsx';
+import Createorder from './features/order/CreateOrder.jsx';
+import Order from './features/order/Order.jsx';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
+  { path: '/', element: <Home /> },
+  { path: '/menu', element: <Menu /> },
+  { path: '/cart', element: <Cart /> },
+  { path: '/order/new', element: <Createorder /> },
+  { path: '/order/:orderId', element: <Order /> },
 ]);
 
 function App() {
