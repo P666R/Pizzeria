c5813e1d4a2dc89d0ce3ba64a68f8282be508247
PRoy <sam469roger@gmail.com>
2024-06-11 23:42:45 +0530

    implemented and integrated AppLayout component

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Pizzeria Co.</Link>
+
+      <p>Sam Roger</p>
+    </header>
+  );
+}
+
+export default Header;
